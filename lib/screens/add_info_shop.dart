cea27fc9fda983eafca4bf07d732b660a51c5e34
Tassaneewan Wongwai <tae60017186@gmail.com>
2021-06-10 03:34:08 +0700

    add images

--- lib/screens/add_info_shop.dart
+++ lib/screens/add_info_shop.dart
@@ -1,6 +1,8 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_appfood/utility/my_style.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:location/location.dart';
 
 class AddInfoShop extends StatefulWidget {
@@ -10,6 +12,9 @@ class AddInfoShop extends StatefulWidget {
 
 class _AddInfoShopState extends State<AddInfoShop> {
   double lat, lng;
+  File _image;
+  final picker = ImagePicker();
+  String nameShop, address, phone;
 
   @override
   void initState() {
@@ -66,7 +71,14 @@ class _AddInfoShopState extends State<AddInfoShop> {
       width: MediaQuery.of(context).size.width,
       child: RaisedButton.icon(
         color: MyStyle().primaryColor,
-        onPressed: () {},
+        onPressed: () {
+          if (nameShop == null ||
+              nameShop.isEmpty ||
+              address == null ||
+              address.isEmpty ||
+              phone == null ||
+              phone.isEmpty) {}
+        },
         icon: Icon(Icons.save),
         label: Text('Save Infomation'),
       ),
@@ -78,7 +90,7 @@ class _AddInfoShopState extends State<AddInfoShop> {
       Marker(
         markerId: MarkerId('myShop'),
         position: LatLng(lat, lng),
-        infoWindow: InfoWindow(title: 'ร้านของคุณ!!', snippet: '$lat , $lng')
+        infoWindow: InfoWindow(title: 'ร้านของคุณ!!', snippet: '$lat , $lng'),
       )
     ].toSet();
   }
@@ -111,22 +123,48 @@ class _AddInfoShopState extends State<AddInfoShop> {
                 Icons.add_a_photo,
                 size: 36.0,
               ),
-              onPressed: () {},
+              onPressed: () => getImageCa(),
             ),
             Container(
               width: 250.0,
-              child: Image.asset('images/shop1.png'),
+              child: _image == null
+                  ? Image.asset('images/shop1.png')
+                  : Image.file(_image),
             ),
             IconButton(
               icon: Icon(
                 Icons.add_photo_alternate,
                 size: 36.0,
               ),
-              onPressed: () {},
+              onPressed: () => getImageGa(),
             ),
           ],
         ),
       );
+
+  Future getImageCa() async {
+    final pickedFile = await picker.getImage(source: ImageSource.camera);
+
+    setState(() {
+      if (pickedFile != null) {
+        _image = File(pickedFile.path);
+      } else {
+        print('No image selected.');
+      }
+    });
+  }
+
+  Future getImageGa() async {
+    final pickedFile = await picker.getImage(source: ImageSource.gallery);
+
+    setState(() {
+      if (pickedFile != null) {
+        _image = File(pickedFile.path);
+      } else {
+        print('No image selected.');
+      }
+    });
+  }
 
   Widget nameForm() => Row(
         mainAxisAlignment: MainAxisAlignment.center,
@@ -134,6 +172,7 @@ class _AddInfoShopState extends State<AddInfoShop> {
           Container(
             width: 250.0,
             child: TextField(
+              onChanged: (value) => nameShop = value.trim(),
               decoration: InputDecoration(
                 labelText: 'ชื่อร้านค้า',
                 prefixIcon: Icon(Icons.account_box),
@@ -150,6 +189,7 @@ class _AddInfoShopState extends State<AddInfoShop> {
           Container(
             width: 250.0,
             child: TextField(
+              onChanged: (value) => address = value.trim(),
               keyboardType: TextInputType.phone,
               decoration: InputDecoration(
                 labelText: 'ที่อยู่ร้านค้า',
@@ -167,6 +207,7 @@ class _AddInfoShopState extends State<AddInfoShop> {
           Container(
             width: 250.0,
             child: TextField(
+              onChanged: (value) => phone = value.trim(),
               decoration: InputDecoration(
                 labelText: 'เบอร์ติดต่อร้านค้า',
                 prefixIcon: Icon(Icons.phone),
