1d7e231c0a0272a32c89528cd8e50ccb6d444176
Tassaneewan Wongwai <tae60017186@gmail.com>
2021-06-11 01:21:09 +0700

    add status data nameshop

--- lib/widget/infomation_shop.dart
+++ lib/widget/infomation_shop.dart
@@ -1,6 +1,11 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_appfood/model/user_model.dart';
 import 'package:flutter_appfood/screens/add_info_shop.dart';
 import 'package:flutter_appfood/utility/my_style.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class InfomationShop extends StatefulWidget {
   @override
@@ -8,6 +13,34 @@ class InfomationShop extends StatefulWidget {
 }
 
 class _InfomationShopState extends State<InfomationShop> {
+  UserModel userModel;
+
+  @override
+  void initState() {
+    super.initState();
+    readDataUser();
+  }
+
+  Future<Null> readDataUser() async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    String id = preferences.getString('id');
+
+    String url =
+        'http://localhost/FlutterFood/getUserWhereId.php?isAdd=true&id=$id';
+    await Dio().get(url).then((value) {
+      //print('value = $value');
+      var result = json.decode(value.data);
+      //print('result = $result');
+      for (var map in result) {
+        setState(() {
+          userModel = UserModel.fromJson(map);
+        });
+        print('nameShop = ${userModel.nameShop}');
+        if (userModel.nameShop.isEmpty) {}
+      }
+    });
+  }
+
   void routeToAddInfo() {
     MaterialPageRoute materialPageRoute = MaterialPageRoute(
       builder: (context) => AddInfoShop(),
@@ -19,11 +52,18 @@ class _InfomationShopState extends State<InfomationShop> {
   Widget build(BuildContext context) {
     return Stack(
       children: <Widget>[
-        MyStyle().titleCenter(context, 'ยังไม่มีข้อมูล กรุณาเพิ่มข้อมูลค่ะ'),
+        userModel == null
+            ? MyStyle().showProgress()
+            : userModel.nameShop.isEmpty
+                ? showNoData(context)
+                : Text('Have data'),
         addAndEditButton(),
       ],
     );
   }
+
+  Widget showNoData(BuildContext context) =>
+      MyStyle().titleCenter(context, 'ยังไม่มีข้อมูล กรุณาเพิ่มข้อมูลค่ะ');
 
   Row addAndEditButton() {
     return Row(
@@ -38,11 +78,10 @@ class _InfomationShopState extends State<InfomationShop> {
                 bottom: 16.0,
               ),
               child: FloatingActionButton(
-                child: Icon(Icons.edit),
-                onPressed: () {
-                  routeToAddInfo();
-                }
-              ),
+                  child: Icon(Icons.edit),
+                  onPressed: () {
+                    routeToAddInfo();
+                  }),
             ),
           ],
         ),
--- lib/widget/order_list_shop.dart
+++ lib/widget/order_list_shop.dart
@@ -1,4 +1,7 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_appfood/model/user_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class OrderListShop extends StatefulWidget {
   @override
@@ -6,6 +9,18 @@ class OrderListShop extends StatefulWidget {
 }
 
 class _OrderListShopState extends State<OrderListShop> {
+
+  UserModel userModel;
+
+  @override
+  void initState() { 
+    super.initState();
+    
+  }
+
+  
+
+
   @override
   Widget build(BuildContext context) {
     return Text(
