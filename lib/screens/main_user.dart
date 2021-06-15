bfca9f233d5e2f70f26e8b773ec745289d1a17cb
Tassaneewan Wongwai <tae60017186@gmail.com>
2021-06-16 00:56:14 +0700

    create main user page

--- lib/screens/main_user.dart
+++ lib/screens/main_user.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_appfood/model/user_model.dart';
 import 'package:flutter_appfood/utility/my_style.dart';
 import 'package:flutter_appfood/utility/signout_process.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -10,11 +14,35 @@ class MainUser extends StatefulWidget {
 
 class _MainUserState extends State<MainUser> {
   String nameUser;
+  List<UserModel> userModels = List();
+  List<Widget> shopCards = List();
 
   @override
   void initState() {
     super.initState();
     findUser();
+    readShop();
+  }
+
+  Future<Null> readShop() async {
+    String url =
+        'http://localhost/FlutterFood/getUserWhereChooseType.php?isAdd=true&ChooseType=Shop';
+    await Dio().get(url).then((value) {
+      // print('value = $value');
+      var result = json.decode(value.data);
+      for (var map in result) {
+        UserModel model = UserModel.fromJson(map);
+       
+        String nameShop = model.nameShop;
+        if (nameShop.isNotEmpty) {
+          print('nameShop = ${model.nameShop}');
+          setState(() {
+            userModels.add(model);
+            shopCards.add(createCard(model));
+          });
+        }
+      }
+    });
   }
 
   Future<Null> findUser() async {
@@ -37,6 +65,12 @@ class _MainUserState extends State<MainUser> {
         ],
       ),
       drawer: showDrawer(),
+      body: shopCards.length == 0
+          ? MyStyle().showProgress()
+          : GridView.extent(
+              maxCrossAxisExtent: 150.0,
+              children: shopCards,
+            ),
     );
   }
 
@@ -48,7 +82,6 @@ class _MainUserState extends State<MainUser> {
         ),
       );
 
-  
   UserAccountsDrawerHeader showHead() {
     return UserAccountsDrawerHeader(
       decoration: MyStyle().myBoxDecoration('draw1.jpg'),
@@ -60,6 +93,21 @@ class _MainUserState extends State<MainUser> {
       accountEmail: Text(
         'Login',
         style: TextStyle(color: MyStyle().primaryColor),
+      ),
+    );
+  }
+
+  Widget createCard(UserModel userModel) {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          Container(
+            width: 80.0,
+            height: 80.0,
+            child: Image.network('http://localhost/${userModel.urlPicture}'),
+          ),
+          Text(userModel.nameShop),
+        ],
       ),
     );
   }
